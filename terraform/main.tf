4d04e1495ea9b33d0d7d60285f30ce324fcd18c1
Rakesh Irukula <58940709+rakeshirukula@users.noreply.github.com>
2024-12-17 15:17:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,33 +44,16 @@ resource "aws_instance" "master" {
   }
 }
 
-resource "aws_instance" "agent" {
-  ami           = "ami-01816d07b1128cd2d"  # Amazon Linux 2 AMI (replace with your desired AMI)
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
-
-  user_data = file("userdata_agent.sh")
-
-  tags = {
-    Name = "agent-node"
-  }
-}
-
-
-
 
 resource "null_resource" "upload_kubeconfig" {
   provisioner "local-exec" {
     command = "aws s3 cp /root/.kube/config s3://${var.s3_bucket_name}/kubeconfig"
   }
 
-  depends_on = [aws_instance.master, aws_instance.agent]
+  depends_on = [aws_instance.master]
 }
 
 output "master_public_ip" {
   value = aws_instance.master.public_ip
 }
 
-output "agent_public_ip" {
-  value = aws_instance.agent.public_ip
-}
